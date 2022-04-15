15f491483ed355a39401b6c71381f2d37a593db0
21BCE0192 <abhinavlv@yahoo.com>
2022-04-15 16:49:02 +0530

    finished section page

---
+++ Section Page/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// document.querySelector('body').addEventListener('click',function(){
+//     document.querySelector('body').style.backgroundColor='#222';
+// });
+
+console.log('WTF');
