1fcf840c84f0fbc26f92bc889bd52dd2e306c4d9
21BCE0192 <abhinavlv@yahoo.com>
2022-04-16 19:18:24 +0530

    final changes

--- Section Page/script.js
+++ Section Page/script.js
@@ -1,7 +1,33 @@
 'use strict';
+// document.querySelector('.tag').addEventListener('click',function(){
+//     document.querySelector('.tag').style.backgroundColor='#402f7c';
+//     document.querySelector('.tag').style.color='#e4d8ff';
 
-// document.querySelector('body').addEventListener('click',function(){
-//     document.querySelector('body').style.backgroundColor='#222';
+//     // if(document.querySelector('.tag').style.backgroundColor==='#e4d8ff'){
+//     //     document.querySelector('.tag').style.backgroundColor='#402f7c';
+//     //     document.querySelector('.tag').style.color='#e4d8ff';
+//     // }
+
+//     // else if(document.querySelector('.tag').style.backgroundColor==='#402f7c'){
+//     //     document.querySelector('.tag').style.backgroundColor='#e4d8ff';
+//     //     document.querySelector('.tag').style.color='#402f7c';
+//     // }
 // });
 
-console.log('WTF');
+let tag_state=0;
+
+document.querySelector(".tags").addEventListener("click", function(e) {
+	if(e.target && e.target.nodeName == "BUTTON") {
+        if(tag_state==1){
+            e.target.style.backgroundColor='#e4d8ff';
+            e.target.style.color='#402f7c';
+            tag_state=0;
+        }
+
+        else if(tag_state==0){
+            e.target.style.backgroundColor='#402f7c';
+            e.target.style.color='#e4d8ff';
+            tag_state=1;
+        }
+	}
+});
